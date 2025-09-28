ffec6e42a810bb94d80d7c29e0ba8a575fda7fc8
Lady-Byron <samantha-everett@outlook.com>
2025-09-28 17:00:07 -0400

    Update Frontend.php

--- src/Content/Frontend.php
+++ src/Content/Frontend.php
@@ -27,14 +27,26 @@ class Frontend
         $this->settings = $settings;
     }
 
-    public function __invoke(Document $document)
+    public function __invoke(Document $document): void
     {
-        $faType = $this->settings->get('blomstra-fontawesome.type');
+        $type = (string) $this->settings->get('blomstra-fontawesome.type', 'free');
+        $kitUrlRaw = (string) $this->settings->get('blomstra-fontawesome.kitUrl', '');
+        $kitUrl = trim($kitUrlRaw);
 
-        if ($faType === 'kit') {
-            $kitUrl = htmlspecialchars($this->settings->get('blomstra-fontawesome.kitUrl'));
-
-            $document->head[] = "<script src='$kitUrl' crossorigin='anonymous'></script>";
+        if ($type === 'kit' && $kitUrl !== '') {
+            // 如果是 .css，使用 <link> 注入；否则（通常是 .js kit）使用 <script>
+            if (preg_match('/\.css(\?.*)?$/i', $kitUrl)) {
+                $href = htmlspecialchars($kitUrl, ENT_QUOTES, 'UTF-8');
+                $document->head[] = '<link rel="stylesheet" href="'.$href.'" />';
+            } else {
+                $src = htmlspecialchars($kitUrl, ENT_QUOTES, 'UTF-8');
+                $document->head[] = '<script src="'.$src.'" crossorigin="anonymous"></script>';
+            }
+            return;
         }
+
+        // 兜底：加载本地 FA7 CSS（放在 fontawesome-6-free/css/ 下以最小改动兼容）
+        $localHref = '/assets/extensions/blomstra-fontawesome/fontawesome-6-free/css/all.min.css';
+        $document->head[] = '<link rel="stylesheet" href="'.htmlspecialchars($localHref, ENT_QUOTES, 'UTF-8').'" />';
     }
 }
